38cd44ad877b8c8bf7a9c1942bf924d332f2f0a6
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2020-03-05 21:39:02 -0500

    Desktop Draft Finished

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import About from '../components/About'
 import Services from '../components/Services'
 import Testimonials from '../components/Testimonials'
 import Blog from '../components/Blog'
+import Footer from '../components/Footer'
 
 export default function Home() {
 	return (
@@ -29,7 +30,7 @@ export default function Home() {
 			</main>
 
 			<footer>
-				Footer
+				<Footer />
 			</footer>
 		</div>
 	)
