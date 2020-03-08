1715428faa02d1b1e2b22f137f08c83301822fa2
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2020-03-07 20:12:44 -0500

    Overlay Started

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+
 function scrollToTop() {
 	window.scroll({
 		top: 0,
@@ -33,24 +35,52 @@ function scrollToBlog() {
 	});
 }
 
-const Header = () => (
-    <div className="header">
-        <div className="header__container">
-            <img
-                onClick={scrollToTop}
-                className="header__logo"
-                src="/simprodos_logo.png"
-                alt="Logo"
-            />
-            <ul className="header__list">
-                <li className="header__item" onClick={scrollToAbout}>About</li>
-                <li className="header__item" onClick={scrollToServices}>Services</li>
-                <li className="header__item" onClick={scrollToTestimonials}>Testimonals</li>
-                <li className="header__item" onClick={scrollToBlog}>Blog</li>
-                <a href="mailto:simprodos@gmail.com" className="header__button">Contact</a>
-            </ul>
-        </div>
-    </div>
-)
+export default class Header extends Component {
+	state = {
+		menuOpen: false,
+	}
 
-export default Header;
+	onMenuOpen = () => {
+		this.setState({
+			...this.state,
+			menuOpen: !this.state.menuOpen,
+		})
+	}
+
+	render() {
+		return (
+			<div className="header">
+				<div className="header__container">
+
+					{this.state.menuOpen ? <div className="header__overlay">
+						<ul>
+							<li>Test</li>
+							<li>Test2</li>
+						</ul>
+					</div> : null}
+
+					<div className="header__menu" onClick={this.onMenuOpen}>
+						<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+							<title>menu</title>
+							<path d="M2 6h28v6h-28zM2 14h28v6h-28zM2 22h28v6h-28z"></path>
+						</svg>
+					</div>
+				
+					<img
+						onClick={scrollToTop}
+						className="header__logo"
+						src="/simprodos_logo.png"
+						alt="Logo"
+					/>
+					<ul className="header__list">
+						<li className="header__item" onClick={scrollToAbout}>About</li>
+						<li className="header__item" onClick={scrollToServices}>Services</li>
+						<li className="header__item" onClick={scrollToTestimonials}>Testimonals</li>
+						<li className="header__item" onClick={scrollToBlog}>Blog</li>
+						<a href="mailto:simprodos@gmail.com" className="header__button">Contact</a>
+					</ul>
+				</div>
+			</div>
+		)
+	}
+}
