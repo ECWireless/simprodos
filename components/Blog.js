0c6f73fcda3799184637c6c5d2d4fbb04828367f
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2020-03-20 17:59:06 -0400

    Blog added

--- components/Blog.js
+++ components/Blog.js
@@ -33,16 +33,22 @@ const Blog = () => (
         <div className="blog__triangle-1" />
         <div className="blog__triangle-2" />
 
-        <div className="blog__card">
-            <div className="blog__card-banner" />
-            <h4 className="blog__title">New years' resolutions</h4>
-            <p className="blog__excerpt">It’s January, a new year full of unlimited
-                potential.  Since the time of the
-                Babylonians, people have made hopes
-                and promises concerning the new
-                year…
-            </p>
-        </div>
+        <a
+            className="blog__card-link"
+            href="https://medium.com/@elliottconway/new-years-resolutions-4e34e7c5a1c8"
+            target="_blank"
+        >
+            <div className="blog__card">
+                <div className="blog__card-banner" />
+                <h4 className="blog__title">New years' resolutions</h4>
+                <p className="blog__excerpt">It’s January, a new year full of unlimited
+                    potential.  Since the time of the
+                    Babylonians, people have made hopes
+                    and promises concerning the new
+                    year…
+                </p>
+            </div>
+        </a>
     </div>
 )
 
